ceae240ee03fc1497d51590f4bbf84dd1e8698bb
Taomosquito <S_Peloquin123@live.com>
2024-10-20 21:22:00 -0400

    -complete exercise_4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,9 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
 def change_store_name(store_id, new_name)
-  store = Store.find(store_id) # Find the store by ID
-  store.update(name: new_name)  # Update the name and save it
+  store = Store.find(store_id)
+  store.update(name: new_name)
 end
 
 change_store_name(1, "Vancouver")
